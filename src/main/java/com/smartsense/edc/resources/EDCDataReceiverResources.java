14417c42f4b3a9537e5ec4b4d487eb8b01795202
DNS Manager <dilip.dhankecha@smartsensesolutions.com>
2022-05-17 15:31:54 +0530

    Manage data

--- src/main/java/com/smartsense/edc/resources/EDCDataReceiverResources.java
+++ src/main/java/com/smartsense/edc/resources/EDCDataReceiverResources.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class EDCDataReceiverResources {
@@ -15,31 +16,30 @@ public class EDCDataReceiverResources {
     private final ObjectMapper objectMapper;
 
     private final static Map<String, Object> assetData = new HashMap<>();
+    private final static Map<String, Object> consumerAssetData = new HashMap<>();
 
     public EDCDataReceiverResources(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
 
-    @PostMapping("/consumer/data/test")
-    public String consumerDataHook(@RequestBody  String data) throws IOException {
+    @PostMapping("/consumer/data/{assetId}")
+    public String consumerDataHook(@PathVariable("assetId") String assetId,@RequestBody  String data) throws IOException {
         System.out.println("DATA GET SUCCESSFULLY.."+data);
-//        Map<String, Object> asset = objectMapper.readValue(data,Map.class);
-//        Map<String, String> properties = (Map<String, String>) asset.get("properties");
-//        String assetId = properties.get("asset:prop:id").toString();
-//        assetData.put(assetId, asset);
+        consumerAssetData.put(assetId,data);
         return "Got it!";
     }
 
-    @GetMapping("/provider/data/webhook")
-    public String providerDataHook() throws IOException {
+    @GetMapping("/provider/data/webhook/{assetId}")
+    public String providerDataHook(@PathVariable("assetId") String assetId) throws IOException {
         System.out.println("READ DATA FROM PROVIDER..");
-        String data = objectMapper.writeValueAsString(Map.of("name", "dilip", "surname", "dhankecha"));
+        assetData.put(assetId,Map.of("AssetId",assetId,"Uuid", UUID.randomUUID().toString(),"Name","SacalonCP-"+UUID.randomUUID().toString()));
+        String data = objectMapper.writeValueAsString(assetData.get(assetId));
         return data;
     }
 
     @GetMapping("asset/{assetId}")
     public ResponseEntity<Object> getAssetData(@PathVariable("assetId") String assetId){
-        if(assetData.containsKey(assetId)){
+        if(consumerAssetData.containsKey(assetId)){
             return ResponseEntity.status(HttpStatus.OK).body(assetData.get(assetId));
         }else{
             return ResponseEntity.status(HttpStatus.OK).body(new HashMap<>());
