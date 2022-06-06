d18868dde521f89dc9d52cf9f4ab13739b6d83b5
Ravi <ravi.ghadiya@smartsensesolutions.com>
2022-06-06 11:03:39 +0530

    added Repository annotation

--- src/main/java/com/smartsense/edc/Repository/ConsumerRepository.java
+++ src/main/java/com/smartsense/edc/Repository/ConsumerRepository.java
@@ -2,6 +2,8 @@ package com.smartsense.edc.Repository;
 
 import com.smartsense.edc.entity.Consumer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConsumerRepository extends JpaRepository<Consumer, String> {
 }
